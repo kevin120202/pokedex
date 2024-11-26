d9b790da65844e535f50681bee6e971c19314b00
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-11-26 17:34:03 -0600

    added repl terminal and test case for cleanInput

---
+++ repl.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+
+		fmt.Println("echoing: ", cleaned)
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
